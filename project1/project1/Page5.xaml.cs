40232eb32c11ca9d6dc679d79cbb826adc40a1af
SpeedWagon12 <151126812+SpeedWagon12@users.noreply.github.com>
2023-11-29 21:39:26 +0300

    Add files via upload

---
+++ project1/project1/Page5.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace project1
+{
+    /// <summary>
+    /// Логика взаимодействия для Page5.xaml
+    /// </summary>
+    public partial class Page5 : Page
+    {
+        public Page5()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void myCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            int z = 145;
+            Answer.Text +=  $" {z} +" ;
+
+        }
+
+        private void myCheckBox_Checked_1(object sender, RoutedEventArgs e)
+        {
+            int x = 75;
+            Answer.Text += $" {x} +";
+        }
+        private void myCheckBox_Checked_2(object sender, RoutedEventArgs e)
+        {
+            int c = 20;
+            Answer.Text += $" {c} +";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            box1.Clear();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Page6());
+        }
+
+        private void myCheckBox_Checked_3(object sender, RoutedEventArgs e)
+        {
+            int q = 20;
+            Answer.Text += $" {q}" + "\n";
+        }
+        private void myCheckBox_Checked_4(object sender, RoutedEventArgs e)
+        {
+            int w = 45;
+            Answer.Text += $" {w}" + "\n";
+        }
+    }
+}
---
+++ project1/project1/Page7.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace project1
+{
+    /// <summary>
+    /// Логика взаимодействия для Page7.xaml
+    /// </summary>
+    public partial class Page7 : Page
+    {
+        public Page7()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_1_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Page5());
+        }
+
+        private void Button_4_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Вы еще не участвовали в марафоннах");
+        }
+
+        private void Button_2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
